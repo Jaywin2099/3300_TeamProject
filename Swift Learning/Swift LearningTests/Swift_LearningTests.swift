7c6f4465884b696f7c5c9f01254f7c1d75c7618c
Jacob Winter <jaywin2099@gmail.com>
2025-02-26 11:16:41 -0600

    Created xcode project

---
+++ Swift Learning/Swift LearningTests/Swift_LearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_LearningTests.swift
+//  Swift LearningTests
+//
+//  Created by Jacob Winter on 2/26/25.
+//
+
+import Testing
+@testable import Swift_Learning
+
+struct Swift_LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
