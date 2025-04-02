4da94806076fdb6cceefde4bb394dea2e2a5e9e5
jaywin2099 <jaywin2099@gmail.com>
2025-04-02 11:50:58 -0500

    seperates ContentView file into FactView and WorldView files

---
+++ SwiftLearning/SwiftLearning/FactView.swift
@@ -0,0 +1,8 @@
+//
+//  FactView.swift
+//  SwiftLearning
+//
+//  Created by Jacob Winter on 4/2/25.
+//
+
+import Foundation
---
+++ SwiftLearning/SwiftLearning/FeatureReader.swift
@@ -0,0 +1,6 @@
+//
+// creates a reader that scans the AR view
+// and can be read by 
+//
+
+import Foundation
