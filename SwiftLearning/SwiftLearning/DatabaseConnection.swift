a19145b49128bee36a12d293d508a164c8d58dc3
Colin Seper <sepercolin@gmail.com>
2025-04-13 14:57:50 -0500

    Add: Updated project dependencies and tested reads from Firebase using authentication. Commented out because of changes I'll make to sign-in and sign-up.

--- SwiftLearning/SwiftLearning/DatabaseConnection.swift
+++ SwiftLearning/SwiftLearning/DatabaseConnection.swift
@@ -3,20 +3,30 @@
 //  SwiftLearning
 //
 
-import Foundation
 import FirebaseCore
+import FirebaseAuth
 import FirebaseFirestore
 
 struct DatabaseConnection {
     private static var isConfigured : Bool = false
-    let db : Firestore
-    
+    private let db : Firestore
+
     init() {
         if !DatabaseConnection.isConfigured {
             FirebaseApp.configure()
             DatabaseConnection.isConfigured = true
         }
         
+        Auth.auth().signIn(withEmail: "", password: "") { result, error in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
+        
         db = Firestore.firestore()
+    }
+    
+    func getDatabase() -> Firestore {
+        return db
     }
 }
